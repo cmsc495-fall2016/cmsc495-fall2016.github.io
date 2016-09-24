de74936bc2a8c0741d6c74128c284e9178bdf091
Adam Howell <ahowell82@gmail.com>
2016-09-24 12:47:57 -0400

    Fix Page parent class
    Fixing the incorrect usage of JFrame instead of JPanel

--- src/main/java/cmsc495/ui/Page.java
+++ src/main/java/cmsc495/ui/Page.java
@@ -3,27 +3,32 @@
  */
 package cmsc495.ui;
 
-import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * @author Adam Howell, Obinna Ojialor
  * @date   2016-09-20
  */
-public class Page extends JFrame{
+public class Page extends JPanel{
 
   /**
    * Generated serial ID
    */
   private static final long serialVersionUID = 8490689814447716962L;
-
+  private String title;
+  
   /**
    * Constructor to contain/set all common things between the pages
-   * NOTE: only enforcing the use of "setTitle" to allow checking 
-   *       of instantiation of JFrame.  use a better method if available
+   * NOTE: only enforcing the use of "setTitle" to allow the border 
+   *       to have a name
    * @param title
    */
   public Page(String title){
-    this.setTitle(title);
+    this.title = title;
   }//end Page
 
+  public String getTitleName(){
+    return this.title;
+  }
+  
 }//end class Page
