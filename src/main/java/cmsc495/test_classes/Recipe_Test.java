b1ec30ae9229e2dd893a1fd9825873151653ddd1
Justin <justin.helphenstine@gmail.com>
2016-09-27 07:51:40 -0400

    Recipe_Test & a revised version of the recipe test data without optional fields. Minor bug exists and is under review.

---
+++ src/main/java/cmsc495/test_classes/Recipe_Test.java
@@ -0,0 +1,130 @@
+package cmsc495.test_classes;
+
+import cmsc495.database.Recipe;
+import com.opencsv.CSVReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Tests the Recipe entity as a standalone. Does not integrate Ingredient testing, also does not populate the 'uses'
+ * table. Requires recipes arrive in a .csv. Support is provided for the 10 fields used by our test_classes inputs.
+ *
+ * @author Justin
+ * @version 0.1
+ */
+public class Recipe_Test {
+
+    File recipe_test_data = null;
+
+
+    private class Recipe_CSV_Entry {
+        int id = -1;
+        String name = null;
+        int serves = -1;
+        String author = null;
+        String prep_time_as_String = null;
+        int prep_time = 0;
+        String cook_time_as_String = null;
+        int cook_time = 0;
+        int difficulty = 0;
+        String procedures = null;
+        String description = null;
+        String source = null;
+    }
+
+    private ArrayList<Recipe_CSV_Entry> testData = new ArrayList<Recipe_CSV_Entry>();
+
+    /**
+     * Utility method used to convert times to minutes for standardization. Admittedly janky algorithm.
+     * @param time  A string containing units of time, e.g. 2.5 hrs, 12 minutes
+     * @return  an integer expressing the given time in minutes
+     */
+    private int ensureTimeInMinutes(String time) {
+        String[] time_elements;
+
+        if (time.contentEquals("")){
+            return 0;
+        }
+        if (time.matches(".+(?<=\\d)(?=\\D)")){ // WHY DOESN'T THIS MATCH ON "10minutes"
+            time_elements = time.split("(?<=\\d)(?=\\D)|(?=\\d)(?<=\\D)");
+            System.out.println(time_elements[0] + " | " + time_elements[1]);
+        } else {
+            time_elements = time.split(" ");
+            System.out.println("DEBUG: " + time_elements[0]);
+            System.out.println(time_elements[0] + " | " + time_elements[1]);
+        }
+
+        int minutes = Integer.parseInt(time_elements[0]);
+        if (time.toLowerCase().contains("min")) {
+            minutes *= 60;
+        }
+        return minutes;
+    }
+
+    /**
+     * Get test_classes data from the .csv; normalize prep / cooking times, create our ArrayList of Recipe elements.
+     * @param file
+     * @throws IOException
+     */
+    private void populateTestData(File file) throws IOException {
+
+        CSVReader reader = null;
+        try {
+            reader = new CSVReader(new FileReader(file));
+            String[] recipe;
+            reader.readNext(); // Get rid of the header line
+            while ((recipe = reader.readNext())!= null) {
+                Recipe_CSV_Entry entry = new Recipe_CSV_Entry();
+                entry.id = Integer.parseInt(recipe[0]);
+                entry.name = recipe[1];
+                entry.serves = Integer.parseInt(recipe[2].split(" ")[0]); // in case our Serves field has non-nummeric data.
+                entry.author = recipe[3];
+                entry.prep_time_as_String = recipe[4];
+                entry.prep_time = ensureTimeInMinutes(entry.prep_time_as_String);
+                entry.cook_time_as_String = recipe[5];
+                entry.cook_time = ensureTimeInMinutes(entry.cook_time_as_String);
+                if (recipe[6].contentEquals("")) {
+                    entry.difficulty = 0;
+                } else {
+                    entry.difficulty = Integer.parseInt(recipe[6]);
+                }
+                entry.procedures = recipe[7];
+                entry.description = recipe[8];
+                entry.source = recipe[9];
+
+                testData.add(entry);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Store our test_classes data into the Recipe table of our database.
+     * @throws SQLException     Standard SQL Exception
+     */
+    private void updateRecipeTable() throws SQLException {
+        for (Recipe_CSV_Entry entry : testData){
+            Recipe r = new Recipe();
+            r.createRecipe(entry.name, entry.serves, entry.author, entry.prep_time, entry.cook_time, entry.difficulty,
+                    entry.procedures, entry.description, entry.source, null);
+        }
+    }
+
+
+    public static void main( String[] args ) throws IOException, SQLException, URISyntaxException {
+        Recipe_Test rt = new Recipe_Test();
+        System.out.println("[!] Begin ingestion of Recipe test_classes data.");
+        rt.populateTestData(new File("src\\main\\java\\cmsc495\\test_data\\recipe_data_optionals_removed.csv"));
+        System.out.println("[!] Test data read in; attempting to write to table");
+        rt.updateRecipeTable();
+        System.out.println("[!] If no stacktrace, assume db recipe table is populated.");
+    }
+}
