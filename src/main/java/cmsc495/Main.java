507bc628c13a21b45e146792a2b78ca304ed46fa
Adam Howell <ahowell13@student.umuc.edu>
2016-09-28 21:09:28 -0400

    Remove the Database call in the main
    The main should only do one thing ... main it up

--- src/main/java/cmsc495/Main.java
+++ src/main/java/cmsc495/Main.java
@@ -3,7 +3,6 @@
  */
 package cmsc495;
 
-import cmsc495.database.Database;
 import cmsc495.ui.SimpleGui;
 
 /**
@@ -13,11 +12,10 @@ import cmsc495.ui.SimpleGui;
 public class Main {
 
   public static void main(String[] args) {
+
     SimpleGui gui = new SimpleGui();
     gui.setVisible(true);
     
-    //Create database and tables
-    Database recipe = new Database();
   }
   
 }//end class Main
